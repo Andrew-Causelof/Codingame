eca938255036b20204d68677624de8fd8789c1de
Andrew -Causelof <akozlov@rocketmail.com>
2020-03-19 13:59:10 +0300

    Chuck Norris problem is solved

--- Chuck-Norris/Chuck-Norris.rb
+++ Chuck-Norris/Chuck-Norris.rb
@@ -2,26 +2,50 @@
 # the standard input according to the problem statement.
 
 message = gets.chomp
-binary = 0
 
-def codepoint(char)
-    char.ord
-end
-
+# convering number to binary code
 def convert_to_binary(integer)
   array = []
   while integer > 0
     array << integer % 2
     integer /= 2
   end
-  array.reverse.join
+  
+  #Checking for 7 bits
+  array.push(0) if array.length == 6
+  #Returning reversed array
+  array.reverse
 end
 
-binary = convert_to_binary(codepoint(message))
+letter = []
 
-# Write an action using puts
-# To debug: STDERR.puts "Debug messages..."
-STDERR.puts "#{binary}"
+# we are goign to split each char in gotten message and then convert to binary array
+message.split('').each { |char| 
+  
+   letter += convert_to_binary(char.ord)
+}
+
+#==============MAIN BODY =======================================
+#So far im too lazy to pack it all into one function, so its realy work same way as written below
+@output = ""
+@previous = letter[0] 
+@counter = 0
+
+letter.each{ |i|
+ # first number
+   @output << "00 0" if @counter == 0 && i == 0 
+   @output << "0 0"  if @counter == 0 && i == 1
+ 
+ if  @counter > 0 && i == @previous
+   @output << "0"
+ elsif @counter > 0 && i != @previous
+  @output << " 00 0" if i == 0
+  @output << " 0 0" if i == 1
+ end
+@previous = i
+@counter += 1
+}      
+#==============RESULT HAS BEEN CALCULATED========================
 
 
-puts "answer"
+puts "#{@output}"
